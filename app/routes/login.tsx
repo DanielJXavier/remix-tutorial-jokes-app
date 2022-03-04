0efcbc40eba036038c5ccf100886077d5caf7aa0
Daniel Xavier <dxdanielxavier@gmail.com>
2022-03-04 09:10:59 -0300

    Add register user action

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -8,7 +8,7 @@ import {
 } from "remix";
 
 import { db } from "~/utils/db.server";
-import { login, createUserSession } from "~/utils/session.server";
+import { login, createUserSession, register, } from "~/utils/session.server";
 
 import stylesUrl from "~/styles/login.css";
 
@@ -101,12 +101,17 @@ export const action: ActionFunction = async ({
           formError: `User with username ${username} already exists`,
         });
       }
-      // create the user
-      // create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
+
+      const user = await register({ username, password });
+
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Something went wrong trying to create a new user.`,
+        });
+      }
+      
+      return createUserSession(user.id, redirectTo);
     }
     default: {
       return badRequest({
