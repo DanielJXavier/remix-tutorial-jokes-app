92db01a6c95f4896418e75983aa441a15d033d4b
Daniel Xavier <dxdanielxavier@gmail.com>
2022-03-04 08:10:38 -0300

    Add styles

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,29 @@
-const IndexRoute = () => (
-  <div>Hello Index Route</div>
+import type { LinksFunction } from "remix";
+
+import stylesUrl from "~/styles/index.css";
+
+export const links: LinksFunction = () => [
+  { 
+    rel: "stylesheet",
+    href: stylesUrl 
+  }
+];
+
+const Index = () => (
+  <div className="container">
+    <div className="content">
+      <h1>
+        Remix <span>Jokes!</span>
+      </h1>
+      <nav>
+        <ul>
+          <li>
+            <Link to="jokes">Read Jokes</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
 );
 
-export default IndexRoute;
+export default Index;
