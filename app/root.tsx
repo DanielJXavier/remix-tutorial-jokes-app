7f758f5ba0f26aadfd8b3ccfe2732da6f7e98a3a
Daniel Xavier <dxdanielxavier@gmail.com>
2022-03-04 07:44:11 -0300

    Clean root file and routes

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,16 @@
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "remix";
-import type { MetaFunction } from "remix";
+import { LiveReload } from "remix";
 
-export const meta: MetaFunction = () => {
-  return { title: "New Remix App" };
-};
+const App = () => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <title>Remix: So great, it's funny!</title>
+    </head>
+    <body>
+      Hello world
+      <LiveReload />
+    </body>
+  </html>
+);
 
-export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
-}
+export default App;
