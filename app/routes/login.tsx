732c1d14af98162c64c3eb03bcb87dcdf173f117
Daniel Xavier <dxdanielxavier@gmail.com>
2022-03-04 09:05:07 -0300

    Add session logic

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -8,6 +8,7 @@ import {
 } from "remix";
 
 import { db } from "~/utils/db.server";
+import { login, createUserSession } from "~/utils/session.server";
 
 import stylesUrl from "~/styles/login.css";
 
@@ -78,13 +79,16 @@ export const action: ActionFunction = async ({
 
   switch (loginType) {
     case "login": {
-      // login to get the user
-      // if there's no user, return the fields and a formError
-      // if there is a user, create their session and redirect to /jokes
-      return badRequest({
-        fields,
-        formError: "Not implemented",
-      });
+      const user = await login({ username, password });
+
+      if (!user) {
+        return badRequest({
+          fields,
+          formError: `Username/Password combination is incorrect`,
+        });
+      }
+
+      return createUserSession(user.id, redirectTo);
     }
     case "register": {
       const userExists = await db.user.findFirst({
